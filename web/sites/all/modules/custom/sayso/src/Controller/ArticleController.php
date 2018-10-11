64f1e2cab727b44f3af745e60823b70ccc1ead85
Brian Clincy <bclincy@gmail.com>
2018-10-11 08:11:43 -0400

    Playing with the template

---
+++ web/sites/all/modules/custom/sayso/src/Controller/ArticleController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bclincy
+ * Date: 10/8/18
+ * Time: 11:37 AM
+ */
+
+namespace Drupal\sayso\Controller;
+
+
+use Drupal\Core\Controller\ControllerBase;
+
+class ArticleController extends ControllerBase {
+
+  public function page() {
+    $work = $this->t('hello world');
+    $items = [
+      ['name' => 'Article 1'],
+      ['name' => 'Article 2'],
+      ['name' => 'Article 3'],
+      ['name' => 'Article 4'],
+      ['name' => 'Article 5'],
+    ];
+
+    return
+      [
+      'theme' => 'article_list',
+      '#items' => $items,
+      '#content' => $work,
+      '#title' => 'Our Article List'
+    ];
+  }
+
+  public function hello($name) {
+    die($name);
+
+  }
+
+}
